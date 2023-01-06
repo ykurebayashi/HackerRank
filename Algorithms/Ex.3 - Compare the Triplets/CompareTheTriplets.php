62cef3794c2c918539d6b6be53245f6534c1d7d5
Yngwie Kurebayashi <yngwie.rs2@gmail.com>
2023-01-06 19:33:30 -0300

    Adds solutions for Compare the Triplets

--- Algorithms/Ex.3 - Compare the Triplets/CompareTheTriplets.php
+++ Algorithms/Ex.3 - Compare the Triplets/CompareTheTriplets.php
@@ -1,5 +1,15 @@
 <?php
   function compareTriplets($a, $b) {
-    
+    $score = array(0, 0);
+
+    foreach ($a as $index => $value) {
+      if ($value > $b[$index]) {
+        $score[0]++;
+      } else if ($value < $b[$index]) {
+        $score[1]++;
+      }
+    }
+  
+    return $score;
   }
 ?>
--- Algorithms/Ex.3 - Compare the Triplets/CompareTheTriplets.ts
+++ Algorithms/Ex.3 - Compare the Triplets/CompareTheTriplets.ts
@@ -1,4 +1,9 @@
 function compareTriplets(a: number[], b: number[]): number[] {
+  let score: number[] = [0,0];
 
-  return [];
+  a.map((value, index) => {
+    value > b[index] ? score[0]++ : value < b[index] ? score[1]++ : null;
+  })
+
+  return score;
 }
