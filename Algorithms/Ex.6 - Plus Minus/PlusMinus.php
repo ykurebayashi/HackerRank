389ee8cbe434ea6a2b575b3d7aa291b5320c61f4
Yngwie Kurebayashi <yngwie.rs2@gmail.com>
2023-01-10 22:01:08 -0300

    adds solution for PlusMinus

--- Algorithms/Ex.6 - Plus Minus/PlusMinus.php
+++ Algorithms/Ex.6 - Plus Minus/PlusMinus.php
@@ -1,6 +1,11 @@
 <?php
   function plusMinus($arr) {
-    // Write your code here
-
+    $positiveNumbers = (sizeof(array_filter($arr, function($element) { return $element > 0; })))/sizeof($arr);
+    $negativeNumbers = (sizeof(array_filter($arr, function($element) { return $element < 0; })))/sizeof($arr);
+    $zeros = (sizeof(array_filter($arr, function($element) { return $element === 0; })))/sizeof($arr);
+    
+    echo number_format($positiveNumbers, 6, '.', '') . PHP_EOL;
+    echo number_format($negativeNumbers, 6, '.', '') . PHP_EOL;
+    echo number_format($zeros, 6, '.', '');
   }
 ?>
--- Algorithms/Ex.6 - Plus Minus/PlusMinus.ts
+++ Algorithms/Ex.6 - Plus Minus/PlusMinus.ts
@@ -1,4 +1,9 @@
 function plusMinus(arr: number[]): void {
-  // Write your code here
-
+  const positiveNumbers = (arr.filter(element => element > 0).length)/arr.length;
+  const negativeNumbers = (arr.filter(element => element < 0).length)/arr.length;
+  const zeros = (arr.filter(element => element === 0).length)/arr.length;
+  
+  console.log(positiveNumbers.toFixed(6));
+  console.log(negativeNumbers.toFixed(6));
+  console.log(zeros.toFixed(6));
 }
